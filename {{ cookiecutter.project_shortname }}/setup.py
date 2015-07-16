150f84d740417d9e7659e8d220357094b73ad49b
Jiri Kuncar <jiri.kuncar@gmail.com>
2015-07-16 09:06:59 +0200

    installation: fix missing comma in classifiers
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- {{ cookiecutter.project_shortname }}/setup.py
+++ {{ cookiecutter.project_shortname }}/setup.py
@@ -94,7 +94,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Libraries :: Python Modules',
         "Programming Language :: Python :: 2",
         # 'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
