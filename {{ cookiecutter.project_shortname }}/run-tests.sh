68228d7eef21c90463441afc349eb7e6b63f0149
Sami Hiltunen <git@hiltunen.io>
2016-02-01 16:10:30 +0100

    installation: pydocstyle>=1.0.0
    * Replaces pep257 with pydocstyle. (closes #48)
    
    Signed-off-by: Sami Hiltunen <sami.mikael.hiltunen@cern.ch>

--- {{ cookiecutter.project_shortname }}/run-tests.sh
+++ {{ cookiecutter.project_shortname }}/run-tests.sh
@@ -1,6 +1,6 @@
 {% include 'misc/header.py' %}
 
-pep257 {{ cookiecutter.package_name }} && \
+pydocstyle {{ cookiecutter.package_name }} && \
 isort -rc -c -df **/*.py && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
