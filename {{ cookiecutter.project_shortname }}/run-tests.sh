0e131d3c3e0953db3c26d822fb2c7440d8cd7d22
Krzysztof Nowak <k.nowak@cern.ch>
2016-07-15 11:59:38 +0200

    setup: requirements building naming
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- {{ cookiecutter.project_shortname }}/run-tests.sh
+++ {{ cookiecutter.project_shortname }}/run-tests.sh
@@ -3,7 +3,7 @@
 
 pydocstyle {{ cookiecutter.package_name }} tests && \
 isort -rc -c -df **/*.py && \
-check-manifest --ignore ".travis-*" && \
+check-manifest && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test && \
 sphinx-build -qnNW -b doctest docs docs/_build/doctest
