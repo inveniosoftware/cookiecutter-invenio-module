6a554d7d59901e7bf97b93ed1357c7770c1eca13
David Caro <david@dcaro.es>
2016-08-02 17:49:47 +0200

    tests: use tox for matrix and docker for env
    * Use tox to span the virtualenv matrix instead of directly travis, so
      the developer can run the tests locally.
    
    * Use docker-compose to setup the testing environment, including the
      database, to achieve a really good and easy reproducibility without
      having to setup the databases and different python versions
      locally.
    
    Signed-off-by: David Caro <david@dcaro.es>

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 chmod a+x run-tests.sh
 
@@ -9,4 +9,21 @@ echo "Skeleton generated."
 echo "Please fix the following TODOs before you use the generated files:"
 grep --color=always --recursive --context=3 --line-number TODO .
 
-rm -r misc/ 
+rm -r misc/
+
+
+# Create the docker templates
+
+
+python_versions='{{cookiecutter.python_versions}}'
+python_versions=(${python_versions//,/ })
+for version in "${python_versions[@]}"; do
+    full_version="python${version: -1}${version: -2:1}"
+    cat > "tests/Dockerfile.${version}" <<EOD
+FROM ${full_version}
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /code
+RUN pip install --upgrade tox
+WORKDIR /code
+EOD
+done
