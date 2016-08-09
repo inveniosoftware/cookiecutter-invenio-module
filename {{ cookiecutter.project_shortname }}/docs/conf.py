25cc917d2702404a98541ad93b0163d0d0262c34
Javier Delgado <javier.delgado.fernandez@cern.ch>
2016-08-09 11:45:02 +0200

    docs: addition of autodoc configuration
    * Sets the autodoc configuration to insert into the main body of an
      autoclass directive the documentation from the `__init__` method and
      class documentation.
    
    Signed-off-by: Javier Delgado <javier.delgado.fernandez@cern.ch>

--- {{ cookiecutter.project_shortname }}/docs/conf.py
+++ {{ cookiecutter.project_shortname }}/docs/conf.py
@@ -307,3 +307,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# Autodoc configuraton.
+autoclass_content = 'both'
