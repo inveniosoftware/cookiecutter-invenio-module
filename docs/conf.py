7b2cfa1b65ab9a57466a1e189abf9ededc6aa8d3
Alexander Ioannidis <a.ioannidis@cern.ch>
2016-11-29 14:29:00 +0100

    sphinx: intersphinx configuration
    * Adds proper intershpinx_mapping entries.
    
    * Adds a `TODO` marker for intersphinx_mapping.
    
    Signed-off-by: Alexander Ioannidis <a.ioannidis@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -310,7 +310,9 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+}
 
 # Autodoc configuraton.
 autoclass_content = 'both'
