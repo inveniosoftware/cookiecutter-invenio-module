3d3822f99afcf721233bc36bfaf7258fcea1f590
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2017-01-30 10:49:08 +0100

    global: copyright years
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- scripts/repocheck.py
+++ scripts/repocheck.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Cookiecutter - Invenio Module Template
-# Copyright (C) 2016 CERN
+# Copyright (C) 2016, 2017 CERN
 #
 # Cookiecutter - Invenio Module Template is free software; you can
 # redistribute it and/or modify it under the terms of the GNU General Public
