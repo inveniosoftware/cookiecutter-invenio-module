7361ca9a211bf7869c95d9b2106a75afb4516aff
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2017-08-07 10:12:34 +0200

    tests: pytest manual run fix
    * Fixes issues that prevented running individual tests manually.
      (closes #110)

--- {{ cookiecutter.project_shortname }}/run-tests.sh
+++ {{ cookiecutter.project_shortname }}/run-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 {% include 'misc/header.py' %}
+export PYTEST_ADDOPTS='docs tests {{ cookiecutter.package_name }}'
+
 pydocstyle {{ cookiecutter.package_name }} tests docs && \
 isort -rc -c -df && \
 check-manifest --ignore ".travis-*" && \
