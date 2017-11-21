521c7ab60b041ec2e4ae069612f3fad06735ab0f
Krzysztof Nowak <k.nowak@cern.ch>
2017-11-21 10:41:06 +0100

    template: removing examples appending (Flask 0.12.3 issue)
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- {{ cookiecutter.project_shortname }}/docs/conf.py
+++ {{ cookiecutter.project_shortname }}/docs/conf.py
@@ -4,12 +4,8 @@
 from __future__ import print_function
 
 import os
-import sys
 
 import sphinx.environment
-
-# Plug example application into module path
-sys.path.append('examples')
 
 # -- General configuration ------------------------------------------------
 
