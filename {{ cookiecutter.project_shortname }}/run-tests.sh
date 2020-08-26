b9a3d9acff266c50a448c7e8a8a5f1b917ce3ef6
Pablo Panero <pablo.panero@cern.ch>
2020-08-26 16:31:06 +0200

    tests: use docker-services-cli

--- {{ cookiecutter.project_shortname }}/run-tests.sh
+++ {{ cookiecutter.project_shortname }}/run-tests.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 {% include 'misc/header.py' %}
 
+# TODO: Pass the services required by your module to the
+# docker-services-cli e.g. `docker-services-cli up es postgresql redis`
+docker-services-cli up
 pydocstyle {{ cookiecutter.package_name }} tests docs && \
 isort {{ cookiecutter.package_name }} tests --check-only --diff && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python -m pytest
+tests_exit_code=$?
+docker-services-cli down
+exit "$tests_exit_code"
