6aa5827596101d94202c941178e56d322d6d18f8
Pablo Panero <pablo.panero@cern.ch>
2020-08-26 14:05:00 +0200

    tests: update isort flags

--- {{ cookiecutter.project_shortname }}/run-tests.sh
+++ {{ cookiecutter.project_shortname }}/run-tests.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 {% include 'misc/header.py' %}
+
 pydocstyle {{ cookiecutter.package_name }} tests docs && \
-isort -rc -c -df && \
+isort {{ cookiecutter.package_name }} tests --check-only --diff && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test
