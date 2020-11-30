e3ca55f18a3e35ed4d3f5875d9647d408c45295b
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-11-30 17:26:07 +0100

    docs: remove coveralls badge

--- {{ cookiecutter.project_shortname }}/{{ cookiecutter.package_name }}/version.py
+++ {{ cookiecutter.project_shortname }}/{{ cookiecutter.package_name }}/version.py
@@ -5,4 +5,4 @@ This file is imported by ``{{ cookiecutter.package_name }}.__init__``,
 and parsed by ``setup.py``.
 """
 
-__version__ = '1.0.0.dev{{ cookiecutter.year }}0000'
+__version__ = '0.1.0'
