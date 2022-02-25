ae2d0c0d83f1a11105c752f9c29c8ab01a7d203b
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2022-02-25 11:17:46 +0100

    packaging: move from setup.py to setup.cfg

--- {{ cookiecutter.project_shortname }}/{{ cookiecutter.package_name }}/version.py
+++
@@ -1,8 +0,0 @@
-{% include 'misc/header.py' %}
-"""Version information for {{ cookiecutter.project_name }}.
-
-This file is imported by ``{{ cookiecutter.package_name }}.__init__``,
-and parsed by ``setup.py``.
-"""
-
-__version__ = '0.1.0'
