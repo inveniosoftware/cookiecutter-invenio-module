5bd9eb045ca2f81f919a159129c7a20bb6deb39b
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2022-02-25 11:17:46 +0100

    packaging: move from setup.py to setup.cfg

--- {{ cookiecutter.project_shortname }}/{{ cookiecutter.package_name }}/__init__.py
+++ {{ cookiecutter.project_shortname }}/{{ cookiecutter.package_name }}/__init__.py
@@ -2,6 +2,7 @@
 """{{ cookiecutter.description }}"""
 
 from .ext import {{ cookiecutter.extension_class }}
-from .version import __version__
+
+__version__ = '0.1.0'
 
 __all__ = ('__version__', '{{ cookiecutter.extension_class }}')
