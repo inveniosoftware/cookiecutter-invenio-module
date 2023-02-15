7deafd2878b1337cd990e67c25a7425ff11e547e
Alex Ioannidis <a.ioannidis@cern.ch>
2023-02-15 16:45:39 +0100

    global: change license to MIT
    * Closes #144.

--- scripts/repocheck.py
+++ scripts/repocheck.py
@@ -1,25 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of Cookiecutter - Invenio Module Template
-# Copyright (C) 2016, 2017 CERN
+# Copyright (C) 2023 CERN.
 #
-# Cookiecutter - Invenio Module Template is free software; you can
-# redistribute it and/or modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
-#
-# Cookiecutter - Invenio Module Template is distributed in the hope that it
-# will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# Cookiecutter - Invenio Module Template; if not, see
-# <http://www.gnu.org/licenses>.
-#
-# In applying this license, CERN does not waive the privileges and immunities
-# granted to it by virtue of its status as an Intergovernmental Organization
-# or submit itself to any jurisdiction.
+# Cookiecutter - Invenio Module Template is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see LICENSE file for more
+# details.
 
 r"""Script for checking repository against cookiecutter output.
 
